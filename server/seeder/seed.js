58d4c9801ca7eb7370c6f1ec1ede75010520fa35
Lauramars <lauramars85@gmail.com>
2021-03-24 20:34:56 +0100

    config.js added to .gitignore

--- server/seeder/seed.js
+++ server/seeder/seed.js
@@ -3,10 +3,8 @@ const MongoClient = require('mongodb').MongoClient;
 
 async function seedDB() {
   // Connection URL
-  const uri =
-    'mongodb+srv://lauramars85:goldenshoe@cluster0.qz9z6.mongodb.net/golden-shoe-demo?retryWrites=true&w=majority';
-
-  // const dbName = 'golden-shoe-demo';
+  const uri = require('../config').mongoURI;
+  // 'mongodb+srv://lauramars85:goldenshoe@cluster0.qz9z6.mongodb.net/golden-shoe-demo?retryWrites=true&w=majority';
 
   const client = new MongoClient(uri, {
     useNewUrlParser: true,
@@ -18,7 +16,6 @@ async function seedDB() {
     console.log('Connected correctly to server');
 
     const db = client.db('golden-shoe-demo');
-    // const productsCollection = db.collection('products');
 
     const productsMenCollection = client.db('men').collection('products');
     const productsWomenCollection = client.db('women').collection('products');
@@ -26,18 +23,20 @@ async function seedDB() {
     const types = ['Sport', 'Casual', 'Sneaker', 'Heels', 'Flat'];
 
     let products = [];
+
     for (let i = 0; i < types.length; i++) {
       let newProduct = {
         name: faker.commerce.productName(),
         price: faker.commerce.price(),
         color: faker.commerce.color(),
+        material: faker.commerce.productMaterial(),
         type: types[Math.floor(Math.random() * types.length)],
         description: faker.lorem.paragraph(),
-        image: ' http://placeimg.com/640/480/fashion',
+        image: ' http://placeimg.com/640/480/shoes',
       };
       products.push(newProduct);
 
-      console.log(newProduct.type);
+      console.log(newProduct.material);
     }
     productsMenCollection.insertMany(products);
     productsWomenCollection.insertMany(products);
