7995591da6e71c71d664ec0a1641219fd6905161
Lauramars <lauramars85@gmail.com>
2021-03-24 16:13:00 +0100

    Created productsMenCollection & productWomenCollection in seed.js

--- server/seeder/seed.js
+++ server/seeder/seed.js
@@ -20,38 +20,31 @@ async function seedDB() {
     const db = client.db('golden-shoe-demo');
     // const productsCollection = db.collection('products');
 
-    const productsCollection = client.db('categories').collection('products');
+    const productsMenCollection = client.db('men').collection('products');
+    const productsWomenCollection = client.db('women').collection('products');
 
-    const categories = [
-      'Men',
-      'Woman',
-      'Sport',
-      'Casual',
-      'Sneaker',
-      'Heels',
-      'Flat',
-    ];
+    const types = ['Sport', 'Casual', 'Sneaker', 'Heels', 'Flat'];
 
     let products = [];
-    for (let i = 0; i < categories.length; i++) {
+    for (let i = 0; i < types.length; i++) {
       let newProduct = {
         name: faker.commerce.productName(),
         price: faker.commerce.price(),
-        category: categories[Math.floor(Math.random() * categories.length)],
+        color: faker.commerce.color(),
+        type: types[Math.floor(Math.random() * types.length)],
         description: faker.lorem.paragraph(),
-        imgage: ' http://placeimg.com/640/480/fashion',
+        image: ' http://placeimg.com/640/480/fashion',
       };
       products.push(newProduct);
 
-      console.log(newProduct.category);
+      console.log(newProduct.type);
     }
-    productsCollection.insertMany(products);
+    productsMenCollection.insertMany(products);
+    productsWomenCollection.insertMany(products);
     console.log('Databae seeded');
     client.close();
   } catch (err) {
     console.log(err);
-  } finally {
-    return;
   }
 }
 
