1ba2972e97e47848fc3d6bb543139d5281ab2518
Lauramars <lauramars85@gmail.com>
2021-03-31 12:38:37 +0200

    Search By Name search bar implemented

--- client/src/components/womenProducts/Women.js
+++ client/src/components/womenProducts/Women.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Fragment } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { getWomenProducts, setLoading } from '../../actions/productActions';
@@ -13,18 +13,61 @@ const Women = ({ getWomenProducts, setLoading, women, loading }) => {
     // eslint-disable-next-line
   }, []);
 
+  const [text, setText] = useState('');
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setLoading();
+    getWomenProducts(text);
+    setText('');
+  };
+
+  const onChange = (e) => setText(e.target.value);
+
+  const filterByName = () => {
+    return women.filter((woman) => {
+      return text
+        ? woman.name.toLowerCase().indexOf(text.toLowerCase()) !== -1
+        : true;
+    });
+  };
+
   return (
     <Fragment>
       <div className='text-page'>
         <h5>Women</h5>
       </div>
+      <Fragment>
+        <form onSubmit={onSubmit} className='form'>
+          <i className='fas fa-search fa-2x'></i>
+          <input
+            className='formInput'
+            type='text'
+            name='text'
+            placeholder='Search Movies...'
+            value={text}
+            onChange={onChange}
+          />
+        </form>
+        {/* <div className='buttons'>
+          <button
+            type='submit'
+            value='Search'
+            className='btn btn-primary'
+            form='form'
+            onClick={onSubmit}
+          >
+            Search
+          </button>
+        </div> */}
+      </Fragment>
       {loading && women !== null ? (
         <p>Loading...</p>
       ) : (
         <div className='cards'>
           <div className='row'>
             {women.length > 0
-              ? women.map((woman, index) => (
+              ? filterByName(women).map((woman, index) => (
                   <WomanItem key={index} woman={woman} />
                 ))
               : null}
